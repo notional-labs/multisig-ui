babcf59d0214d3b5962799500a8457cc5743ad9a
Master Pi <minh.vln140501@gmail.com>
2022-07-21 10:50:21 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,11 @@
 
 git stash
 git pull
-cp ../env-multisig .env.local
+if [ -f "../env-multisig" ]; then
+    echo "Multisig env file exists."
+    cp ../env-multisig .env.local
+fi
+which npm
 npm install
 npm run build
 systemctl restart multisig.service
