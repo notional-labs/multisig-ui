64a8b4f823348bc517755c37f5c1320d02da867f
sontrinh16 <trinhleson2000@gmail.com>
2022-08-22 02:01:39 +0700

    remove gen proto

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,6 @@
 import "antd/dist/antd.css";
 import { ChainProvider } from "../components/Context";
 import IntroPage from "../components/ulti/IntroPage";
-import { createProto } from "../libs/gen-proto";
-
-export async function getServerSideProps() {
-  await createProto()
-  return {
-    props: { status: "success" }
-  }
-}
 
 export default function Home() {
   return (
