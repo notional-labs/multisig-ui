9aafe8019fb9d7f574fa7607b1a745d614942385
sontrinh16 <trinhleson2000@gmail.com>
2022-09-22 22:58:27 +0700

    change chain data

--- data/experimentalChain.js
+++ data/experimentalChain.js
@@ -2,8 +2,8 @@ export const chainObj = {
     "kava_2222-10": {
         chainId: "kava_2222-10",
         chainName: "kava",
-        rpc: "https://rpc-kava-ia.notional.ventures/",
-        rest: "https://api-kava-ia.notional.ventures/",
+        rpc: "https://rpc-kava-ia.cosmosia.notional.ventures/",
+        rest: "https://api-kava-ia.cosmosia.notional.ventures/",
         bip44: {
             coinType: 118,
         },
@@ -46,8 +46,8 @@ export const chainObj = {
     "omniflixhub-1": {
         chainId: "omniflixhub-1",
         chainName: "omniflixhub",
-        rpc: "https://rpc-omniflixhub-ia.notional.ventures/",
-        rest: "https://api-omniflixhub-ia.notional.ventures/",
+        rpc: "https://rpc-omniflixhub-ia.cosmosia.notional.ventures/",
+        rest: "https://api-omniflixhub-ia.cosmosia.notional.ventures/",
         bip44: {
             coinType: 118,
         },
@@ -90,8 +90,8 @@ export const chainObj = {
     "cheqd-mainnet-1": {
         chainId: "cheqd-mainnet-1",
         chainName: "cheqd",
-        rpc: "https://rpc-cheqd-ia.notional.ventures/",
-        rest: "https://api-cheqd-ia.notional.ventures/",
+        rpc: "https://rpc-cheqd-ia.cosmosia.notional.ventures/",
+        rest: "https://api-cheqd-ia.cosmosia.notional.ventures/",
         bip44: {
             coinType: 118,
         },
@@ -134,8 +134,8 @@ export const chainObj = {
     "bitcanna-1": {
         chainId: "bitcanna-1",
         chainName: "bitcanna",
-        rpc: "https://rpc-bitcanna-ia.notional.ventures/",
-        rest: "https://api-bitcanna-ia.notional.ventures/",
+        rpc: "https://rpc-bitcanna-ia.cosmosia.notional.ventures/",
+        rest: "https://api-bitcanna-ia.cosmosia.notional.ventures/",
         bip44: {
             coinType: 118,
         },
@@ -168,6 +168,94 @@ export const chainObj = {
             coinMinimalDenom: "ubcna",
             coinDecimals: 6,
             coinGeckoId: "bitcanna",
+        },
+        gasPriceStep: {
+            low: 0.01,
+            average: 0.025,
+            high: 0.03,
+        },
+    },
+    "pylons-testnet-3": {
+        chainId: "pylons-testnet-3",
+        chainName: "pylons",
+        rpc: "https://rpc-pylons-ia.cosmosia.notional.ventures/",
+        rest: "https://api-pylons-ia.cosmosia.notional.ventures/",
+        bip44: {
+            coinType: 118,
+        },
+        bech32Config: {
+            bech32PrefixAccAddr: "pylo",
+            bech32PrefixAccPub: "pylo" + "pub",
+            bech32PrefixValAddr: "pylo" + "valoper",
+            bech32PrefixValPub: "pylo" + "valoperpub",
+            bech32PrefixConsAddr: "pylo" + "valcons",
+            bech32PrefixConsPub: "pylo" + "valconspub",
+        },
+        currencies: [
+            {
+                coinDenom: "BEDROCK",
+                coinMinimalDenom: "ubedrock",
+                coinDecimals: 6,
+                coinGeckoId: "pylon",
+            },
+        ],
+        feeCurrencies: [
+            {
+                coinDenom: "BEDROCK",
+                coinMinimalDenom: "ubedrock",
+                coinDecimals: 6,
+                coinGeckoId: "pylon",
+            },
+        ],
+        stakeCurrency: {
+            coinDenom: "BEDROCK",
+            coinMinimalDenom: "ubedrock",
+            coinDecimals: 6,
+            coinGeckoId: "pylon",
+        },
+        gasPriceStep: {
+            low: 0.01,
+            average: 0.025,
+            high: 0.03,
+        },
+    },
+    "gravity-bridge-3": {
+        chainId: "gravity-bridge-3",
+        chainName: "gravity bridge",
+        rpc: "https://rpc-gravitybridge-ia.cosmosia.notional.ventures/",
+        rest: "https://api-gravitybridge-ia.cosmosia.notional.ventures/",
+        bip44: {
+            coinType: 118,
+        },
+        bech32Config: {
+            bech32PrefixAccAddr: "gravity",
+            bech32PrefixAccPub: "gravity" + "pub",
+            bech32PrefixValAddr: "gravity" + "valoper",
+            bech32PrefixValPub: "gravity" + "valoperpub",
+            bech32PrefixConsAddr: "gravity" + "valcons",
+            bech32PrefixConsPub: "gravity" + "valconspub",
+        },
+        currencies: [
+            {
+                coinDenom: "GRAVITON",
+                coinMinimalDenom: "ugraviton",
+                coinDecimals: 6,
+                coinGeckoId: "graviton",
+            },
+        ],
+        feeCurrencies: [
+            {
+                coinDenom: "GRAVITON",
+                coinMinimalDenom: "ugraviton",
+                coinDecimals: 6,
+                coinGeckoId: "graviton",
+            },
+        ],
+        stakeCurrency: {
+            coinDenom: "GRAVITON",
+            coinMinimalDenom: "ugraviton",
+            coinDecimals: 6,
+            coinGeckoId: "graviton",
         },
         gasPriceStep: {
             low: 0.01,
