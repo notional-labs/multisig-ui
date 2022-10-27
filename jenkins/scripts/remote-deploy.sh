7653fa78232cb3402cebf7bd9752fd5f512d598b
yasin <yasin@rnssol.com>
2022-10-27 11:34:30 +0500

    Dockerfile added

---
+++ jenkins/scripts/remote-deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+
+#=====================================
+# HOST_IP  controlled by jenkins env
+# HOST_USER controlled by jenkins envs
+# PORT controlled by jenkins
+#=====================================
+
+# Get current git commit number
+LABEL=$(git log -1 --format=%h)
+
+CONTAINER_NAME=multisig-ui-pylons
+
+CONTAINER_CURRENT=rnssolutions/$CONTAINER_NAME:$LABEL
+    docker stop $CONTAINER_NAME-$BRANCH_NAME
+    docker rm -f $CONTAINER_NAME-$BRANCH_NAME
+    docker run  -d -p $PORT:3000 --name $CONTAINER_NAME-$BRANCH_NAME $CONTAINER_CURRENT
+
