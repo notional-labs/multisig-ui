3ad6c44f6b19c5566ba7cf30bbd49f905811aa07
Faisal Naveed <faisal@rnssol.com>
2022-11-01 02:51:10 +0500

    updated docker file

--- jenkins/scripts/docker-build.sh
+++ jenkins/scripts/docker-build.sh
@@ -8,5 +8,5 @@ LABEL=$(git log -1 --format=%h)
 echo "Build docker image with label "$LABEL
 
 # Build docker of current directory
-docker build --build-arg NEXT_PUBLIC_FAUNADB_SECRET=$NEXT_PUBLIC_FAUNADB_SECRET --build-arg NEXT_PUBLIC_HOST=$NEXT_PUBLIC_HOST -t rnssolutions/multisig-ui-pylons:$LABEL .
+docker build --build-arg NEXT_PUBLIC_FAUNADB_SECRET=$NEXT_PUBLIC_FAUNADB_SECRET --build-arg NEXT_PUBLIC_HOST=$NEXT_PUBLIC_HOST --build-arg NEXT_PUBLIC_CHAIN_ID=$NEXT_PUBLIC_CHAIN_ID --build-arg NEXT_PUBLIC_RPC=$NEXT_PUBLIC_RPC --build-arg NEXT_PUBLIC_REST=$NEXT_PUBLIC_REST -t rnssolutions/multisig-ui-pylons:$LABEL .
 
