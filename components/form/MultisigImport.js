f3655cb2c6503774d7fe7dd399cd36581b8e7793
Son Trinh <sontrinh@Sons-MacBook-Pro.local>
2023-02-15 01:52:42 +0700

    fix address decode

--- components/form/MultisigImport.js
+++ components/form/MultisigImport.js
@@ -4,8 +4,12 @@ import Button from "../input/Button"
 import { getMultisigAccountByAPI } from "../../libs/keplrClient";
 import { openLoadingNotification, openNotification } from "../ulti/Notification"
 import { importMultisigFromPubkeys } from "../../libs/multisig";
-const { toBech32, fromHex } = require("@cosmjs/encoding");
+const { toBech32, fromBase64 } = require("@cosmjs/encoding");
+import {rawSecp256k1PubkeyToRawAddress} from "@cosmjs/tendermint-rpc"
 import { motion } from "framer-motion"
+import FlexRow from "../flex_box/FlexRow"
+import { InputNumber } from "antd"
+import { useRouter } from "next/router"
 
 const style = {
     inputNumberStyle: {
@@ -31,23 +35,27 @@ const MultisigImport = ({ chain }) => {
     const [pubkeys, setPubkeys] = useState([])
     const [components, setComponents] = useState([])
     const [threshold, setThreshold] = useState(0)
+    const router = useRouter()
+
     const handleOnBlur = async () => {
         try {
             if (!address || address === "") {
                 setErr("Address must not be empty")
                 return
             }
+            setErr("")
             const acc = await getMultisigAccountByAPI(chain.api, address)
-            let components = []
+            let componentsAddr = []
             setPubkeys([...acc.pub_key.public_keys])
-            pubkeys.forEach(
+            acc.pub_key.public_keys.map(
                 (item) => {
-                    const addrUint8Array = fromHex(item.key);
-                    const addr = toBech32(prefix, addrUint8Array);
-                    components.push(addr)
+                    const addrUint8Array = fromBase64(item.key)
+                    const rawAddr = rawSecp256k1PubkeyToRawAddress(addrUint8Array)
+                    const addr = toBech32(chain.prefix, rawAddr)
+                    componentsAddr.push(addr)
                 }
             );
-            setComponents([...components])
+            setComponents([...componentsAddr])
             setThreshold(acc.pub_key.threshold)
         }
         catch (e) {
@@ -57,7 +65,7 @@ const MultisigImport = ({ chain }) => {
     }
 
     const handleOnChange = (e) => {
-        setAddress(e.targe.value)
+        setAddress(e.target.value)
     }
 
     const handleImport = async () => {
@@ -66,17 +74,13 @@ const MultisigImport = ({ chain }) => {
                 setErr("Address must not be empty")
                 return
             }
-            let components = []
             const compressedPubkeys = pubkeys.map(
                 (item) => {
-                    const addrUint8Array = fromHex(item.key);
-                    const addr = toBech32(prefix, addrUint8Array);
-                    components.push(addr)
                     return item.key
                 }
             );
 
-            openLoadingNotification("open", "Creating multisig")
+            openLoadingNotification("open", "Importing multisig")
             const multisigAddress = await importMultisigFromPubkeys(
                 compressedPubkeys,
                 parseInt(threshold, 10),
@@ -105,7 +109,9 @@ const MultisigImport = ({ chain }) => {
     return (
         <>
             <Input
-                onChange={handleOnChange}
+                onChange={(e) => {
+                    handleOnChange(e);
+                }}
                 value={address}
                 label="Multisig Address"
                 name="address"
@@ -115,102 +121,141 @@ const MultisigImport = ({ chain }) => {
                 }}
                 error={err}
             />
-            <div>
-                <table
-                    style={{
-                        width: "100%",
-                        borderSpacing: "0 1em",
-                    }}
-                >
-                    <thead
+            {
+                pubkeys.length > 0 && <div>
+                    <h2
                         style={{
-                            borderBottom: "solid 1.25px black",
-                            fontSize: "1.25rem"
+                            marginTop: "20px",
                         }}
                     >
-                        <tr>
-                            <th
-                                style={{
-                                    width: "40%",
-                                    padding: ".5em",
-                                    textAlign: "left"
-                                }}
-                            >
-                                Type
-                            </th>
-                            <th
-                                style={{
-                                    width: "60%",
-                                    padding: ".5em",
-                                    textAlign: "left"
-                                }}
-                            >
-                                Pubkeys
-                            </th>
-                        </tr>
-                    </thead>
-                    <motion.tbody
-                        animate={{
-                            transition: {
-                                staggerChildren: 0.1
+                        Components
+                    </h2>
+                    <table
+                        style={{
+                            width: "100%",
+                            borderSpacing: "0 1em",
+                        }}
+                    >
+                        <thead
+                            style={{
+                                borderBottom: "solid 1.25px black",
+                                fontSize: "1.25rem"
+                            }}
+                        >
+                            <tr>
+                                <th
+                                    style={{
+                                        width: "40%",
+                                        padding: ".5em",
+                                        textAlign: "left"
+                                    }}
+                                >
+                                    Address
+                                </th>
+                                <th
+                                    style={{
+                                        width: "60%",
+                                        padding: ".5em",
+                                        textAlign: "left"
+                                    }}
+                                >
+                                    Pubkey
+                                </th>
+                            </tr>
+                        </thead>
+                        <motion.tbody
+                            animate={{
+                                transition: {
+                                    staggerChildren: 0.1
+                                }
+                            }}
+                        >
+                            {
+                                pubkeys.map((pub, index) => {
+                                    return (
+                                        <motion.tr
+                                            initial={{
+                                                y: 60,
+                                                opacity: 0,
+                                                transition: { duration: .6, ease: [0.6, -0.05, 0.01, 0.99] }
+                                            }}
+                                            animate={{
+                                                y: 0,
+                                                opacity: 1,
+                                                transition: {
+                                                    duration: .6,
+                                                    ease: [0.6, -0.05, 0.01, 0.99]
+                                                }
+                                            }}
+                                            whileHover={{ scale: 1.009 }}
+                                            whileTap={{ scale: 0.95 }}
+                                            key={index}
+                                            style={{
+                                                width: "100%",
+                                                borderBottom: "solid .25px #d6d6d6",
+                                            }}
+                                        >
+                                            <td
+                                                style={{
+                                                    width: "40%",
+                                                    fontSize: "1rem",
+                                                    padding: ".5em"
+                                                }}
+                                            >
+                                                {
+                                                    components[index]
+                                                }
+                                            </td>
+                                            <td
+                                                style={{
+                                                    width: "60%",
+                                                    fontSize: "1rem",
+                                                    padding: ".5em",
+                                                    textAlign: "left"
+                                                }}
+                                            >
+                                                {
+                                                    pub.key
+                                                }
+                                            </td>
+                                        </motion.tr>
+                                    )
+                                })
                             }
+                        </motion.tbody >
+                    </table>
+                    <FlexRow
+                        components={[
+                            <InputNumber
+                                disabled={true}
+                                value={threshold}
+                                style={{
+                                    ...style.inputNumberStyle,
+                                    backgroundColor: "#F5F5F5"
+                                }}
+                            />,
+                            <text
+                                style={{
+                                    fontSize: "1.5rem",
+                                    position: "absolute",
+                                    top: "10px"
+                                }}
+                            >
+                                Of
+                            </text>,
+                            <InputNumber
+                                value={pubkeys.length}
+                                disabled={true}
+                                style={style.inputNumberStyle}
+                            />
+                        ]}
+                        style={{
+                            position: "relative",
+                            marginTop: "20px"
                         }}
-                    >
-                        {
-                            pubkeys.length > 0 && pubkeys.map((pub, index) => {
-                                return (
-                                    <motion.tr
-                                        initial={{
-                                            y: 60,
-                                            opacity: 0,
-                                            transition: { duration: .6, ease: [0.6, -0.05, 0.01, 0.99] }
-                                        }}
-                                        animate={{
-                                            y: 0,
-                                            opacity: 1,
-                                            transition: {
-                                                duration: .6,
-                                                ease: [0.6, -0.05, 0.01, 0.99]
-                                            }
-                                        }}
-                                        whileHover={{ scale: 1.009 }}
-                                        whileTap={{ scale: 0.95 }}
-                                        key={index}
-                                        style={{
-                                            width: "100%",
-                                            borderBottom: "solid .25px #d6d6d6",
-                                        }}
-                                    >
-                                        <td
-                                            style={{
-                                                width: "40%",
-                                                fontSize: "1rem",
-                                                padding: ".5em"
-                                            }}
-                                        >
-                                            {
-                                                components[index]
-                                            }
-                                        </td>
-                                        <td
-                                            style={{
-                                                width: "60%",
-                                                fontSize: "1rem",
-                                                padding: ".5em",
-                                                textAlign: "center"
-                                            }}
-                                        >
-                                            {
-                                                pub.key
-                                            }
-                                        </td>
-                                    </motion.tr>
-                                )
-                            })
-                        }
-                    </motion.tbody >
-                </table>
-            </div>
+                    />
+                </div>
+            }
             <Button
                 text={"IMPORT MULTISIG"}
                 style={{
