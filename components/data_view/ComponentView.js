be09595eb95233545f0309556f1d365e8e7bba32
Son Trinh <sontrinh@Sons-MacBook-Pro.local>
2023-03-01 12:14:10 +0700

    change pubkey to address method

--- components/data_view/ComponentView.js
+++ components/data_view/ComponentView.js
@@ -6,12 +6,24 @@ const ComponentView = ({ pubkey, index, prefix, chain }) => {
     const [address, setAddress] = useState("")
 
     useEffect(() => {
-        const addr = pubkey && pubkeyToAddress(
-            {
-                type: "tendermint/PubKeySecp256k1",
-                value: pubkey,
-            }, prefix)
-        setAddress(addr)
+        if (chain.chain_id.startsWith('evmos')) {
+            const address = PublicKey.fromBase64(fromBase64(pubkey))
+            address.type = '/ethermint.crypto.v1.ethsecp256k1.PubKey'
+            const addr = address.toAddress().toBech32('evmos')
+            setAddress(addr)
+        }
+        else if (chain.chain_id.startsWith('injective')) {
+            const address = PublicKey.fromBase64(fromBase64(pubkey))
+            address.type = '/injective.crypto.v1beta1.ethsecp256k1.PubKey'
+            const addr = address.toAddress().toBech32('inj')
+            setAddress(addr)
+        }
+        else {
+            const addrUint8Array = fromBase64(pubkey)
+            const rawAddr = rawSecp256k1PubkeyToRawAddress(addrUint8Array)
+            const addr = toBech32(prefix, rawAddr)
+            setAddress(addr)
+        }
     }, [pubkey])
 
     return (
