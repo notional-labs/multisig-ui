b90f7e70a7d124b655c3ac96d104232fc9f8733b
sontrinh16 <trinhleson2000@gmail.com>
2023-07-28 23:32:48 +0700

    add manually input

---
+++ components/form/transaction/ExecuteContractMsg.js
@@ -0,0 +1,141 @@
+import { useState } from "react";
+import Input from "../../input/Input"
+import { isValidAddress } from "../../../libs/checkTool";
+import { openNotification } from "../../ulti/Notification";
+import { createSendMsg, } from "../../../libs/transaction";
+import { convertValueFromDenom } from "../../../libs/stringConvert";
+import Button from "../../input/Button";
+import { InputNumber } from 'antd';
+
+const ExecuteContractMsgForm = ({ address, chain, style, msgs, setMsgs }) => {
+    const [txBody, setTxBody] = useState({
+        sender: "",
+        contract: "",
+        msg: any,
+        funds: []
+    })
+    const [addrError, setAddrError] = useState("")
+
+    const disabled = () => {
+        if (invalidForm() || addrError !== "") {
+            return true
+        }
+        return false
+    }
+
+    const handleInputJson = (str) => {
+        try{
+            const msg = JSON.parse(str)
+            setTxBody({
+                ...txBody,
+                msg: msg
+            })
+        }
+        catch (e) {
+            openNotification('error', 'Fail to parse JSON string: ' + e.message)
+        }
+    }
+
+    const handleInputNumber = (val) => {
+        setTxBody({
+            ...txBody,
+            amount: parseFloat(val)
+        })
+    }
+
+    const createMsg = () => {
+        try {
+            const msg = createSendMsg(
+                address,
+                txBody.toAddress,
+                convertValueFromDenom(chain.base_denom, txBody.amount),
+                chain.denom
+            )
+            setMsgs([...msgs, msg])
+            openNotification('success', 'Adding successfully')
+        }
+        catch (e) {
+            openNotification('error', e.message)
+        }
+    }
+
+    const handleKeyGroupChange = (e) => {
+        if(e.target.name === "amount") {
+            setTxBody({
+                ...txBody,
+                [e.target.name]: parseFloat(e.target.value)
+            })
+        }
+        else {
+            setTxBody({
+                ...txBody,
+                [e.target.name]: e.target.value
+            })
+        }
+    }
+
+    const handleKeyBlur = (e) => {
+        if (e.target.name === "toAddress" && !isValidAddress(e.target.value, chain.prefix)) {
+            setAddrError("Invalid Address")
+        }
+        else {
+            setAddrError("")
+        }
+    }
+
+    return (
+        <div>
+            <Input
+                onChange={(e) => {
+                    handleKeyGroupChange(e);
+                }}
+                value={txBody.toAddress}
+                label="Send To"
+                name="toAddress"
+                placeholder="Address here"
+                error={addrError}
+                onBlur={handleKeyBlur}
+                style={style.input}
+            />
+            <div>
+                <h4
+                    style={{
+                        margin: 0
+                    }}
+                >
+                    {`Amount (${chain.denom.substring(1).toUpperCase()})`}
+                </h4>
+                <InputNumber
+                    onChange={handleInputNumber}
+                    value={txBody.amount}
+                    label={`Amount (${chain.denom.substring(1).toUpperCase()})`}
+                    name="amount"
+                    placeholder="Amount"
+                    style={{
+                        ...style.input,
+                        width: "100%",
+                        borderRadius: "10px",
+                        border: "solid 1px black",
+                        padding: "10px 0"
+                    }}
+                />
+            </div>
+            <Button
+                text={"Add Message"}
+                style={{
+                    backgroundColor: disabled() ? "#808080" : "black",
+                    color: "white",
+                    padding: "1em",
+                    width: "100%",
+                    borderRadius: "10px",
+                    marginTop: "20px",
+                    border: 0
+                }}
+                clickFunction={createMsg}
+                disable={disabled()}
+            />
+        </div>
+    )
+}
+
+export default ExecuteContractMsgForm
