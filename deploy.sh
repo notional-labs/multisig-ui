5e1d5d6d09f1ec452d99035fd666797f394fab6e
tungle <anhletung@notional.ventures>
2024-02-19 11:04:44 +0700

    fix cd

--- deploy.sh
+++ deploy.sh
@@ -8,22 +8,11 @@ if ! [ $? -eq 0 ]; then
     exit 1
 fi
 
-if [ -f "../env-multisig" ]; then
-    echo "Multisig env file exists."
-    cp ../env-multisig .env
-fi
-
-cp multisig.notional.ventures.service /etc/systemd/system/multisig.notional.ventures.service
-systemctl stop multisig.notional.ventures.service
-systemctl daemon-reload
-echo "Service file copied!"
-
 npm install
 npm run build
 
 if [ $? -eq 0 ]; then
-    systemctl restart multisig.notional.ventures.service
-    systemctl is-active --quiet multisig.notional.ventures.service && echo "Multisig web server restarted successfully." || (echo "Multisig web server failed to restart." && exit 1)
+    pm2 restart multisig-ui
 else
     echo "npm run build failed with error. Stopped restarting the web server."
     exit 1
